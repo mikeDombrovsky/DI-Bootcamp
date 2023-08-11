42bef009809df3580e7af545f6c40e47f94fa2d5
Mikhail Dombrovsky <madombrovsky@gmail.com>
2023-08-11 23:22:05 +0300

    week5 day4 MiniProject

--- Week5/Day4/MiniProject/script.js
+++ Week5/Day4/MiniProject/script.js
@@ -1,21 +1,65 @@
-const main = document.querySelector("main");
+const field = document.querySelector("main#field");
+let currentColor = "black";
+let isMouseDown = false;
+
+fillField();
+
+const colors = document.querySelectorAll("aside>div");
+const cells = field.querySelectorAll("div");
+
+addEmentListeners();
+addEventListenersToCells();
+
+function addEmentListeners() {
+    for (let color of colors) {
+        color.addEventListener("click", getColor);
+    }
+}
+
+function addEventListenersToCells() {
+    for (let cell of cells) {
+        cell.addEventListener("mousedown", setColor);
+        cell.addEventListener("mouseover", setColorOver);
+        cell.addEventListener("mouseup", stopColor)
+    }
+}
+
+function getColor(e) {
+    currentColor = e.target.style.backgroundColor;
+    console.log(currentColor);
+}
+
+function setColor(e) {
+    isMouseDown = true;
+    e.target.style.backgroundColor = currentColor;
+}
+
+function setColorOver(e) {
+    if (isMouseDown) {
+        e.target.style.backgroundColor = currentColor;
+    }
+}
+
+function stopColor(e) {
+    isMouseDown = false;
+    console.log(isMouseDown);
+}
 
 
-fillMain();
 
-
-
-function fillMain() {
-    for (let i = 0; i < 100; i++) {
+function fillField() {
+    for (let i = 0; i < 5000; i++) {
         let div = document.createElement("div");
         div.style.backgroundColor = "white";
-        
-        main.appendChild(div);
+
+        field.appendChild(div);
     }
 }
 
 
-function clearField(){
-    
+function clearField() {
+    for (let cell of cells) {
+        cell.style.backgroundColor = "white";
+    }
 }
 
