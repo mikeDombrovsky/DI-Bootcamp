5bbbd89141824e920f9b0500b86f93aa05e0e4cb
Mikhail Dombrovsky <madombrovsky@gmail.com>
2023-08-31 13:07:55 +0300

    week8 day4 ExercisesXP ex3-5

--- Week8/Day4/ExercisesXP/ex1.js
+++ Week8/Day4/ExercisesXP/ex1.js
@@ -23,3 +23,63 @@ function displayStudentInfo(objUser) {
 }
 
 displayStudentInfo({ first: 'Elie', last: 'Schoppik' });
+
+//ex3 Using this object
+const users = { user1: 18273, user2: 92833, user3: 90315 }
+
+//1 Using methods taught in class, turn the users object into an array:[ [ 'user1', 18273 ], [ 'user2', 92833 ], [ 'user3', 90315 ] ]
+const users_arr = Object.entries(users);
+console.log(users_arr);
+
+//2 Modify the outcome of part 1, by multipling the user’s ID by 2.
+const users_arr2 = Object.entries(users).map(([username, id]) => [username, id * 2]);
+console.log(users_arr2);
+
+//ex4 Analyze the code below. What will be the output? GUESS: Person, REAL: object.  So.. we need use obj.constructor.name instead
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+const member = new Person('John');
+console.log(typeof member);
+
+//ex5
+class Dog {
+    constructor(name) {
+        this.name = name;
+    }
+};
+// Analyze the options below. Which constructor will successfully extend the Dog class? GUESS - 2 and 4 but 2 uses super properly. By the way 1 also wont throw error.
+// 1
+// class Labrador extends Dog {
+//     constructor(name, size) {
+//         this.size = size;
+//     }
+// };
+// 2
+class Labrador extends Dog {
+    constructor(name, size) {
+        super(name);
+        this.size = size;
+    }
+};
+// 3
+// class Labrador extends Dog {
+//     constructor(size) {
+//         super(name);
+//         this.size = size;
+//     }
+// };
+// 4
+// class Labrador extends Dog {
+//     constructor(name, size) {
+//         this.name = name;
+//         this.size = size;
+//     }
+// };
+
+console.log(new Labrador("killer", 30));
+
+//ex6 Evaluate these (ie True or False)
