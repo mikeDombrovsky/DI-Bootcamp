46b62b84bf647963cee21bdcb176e7eaaba4ca3b
Mikhail Dombrovsky <madombrovsky@gmail.com>
2023-09-10 18:04:28 +0300

    week9 day4 dailyChallenge

--- Week9/Day4/DailyChallenge/script.js
+++ Week9/Day4/DailyChallenge/script.js
@@ -1,6 +1,8 @@
 const form = document.getElementById('search_form');
 form.addEventListener('submit', searchGif);
 const xhr = new XMLHttpRequest();
+const remove_all_btn = document.querySelector('#remove_all');
+remove_all_btn.addEventListener('click', removeAll);
 
 function searchGif(e) {
     e.preventDefault();
@@ -18,21 +20,40 @@ function fetchGif(tag) {
     xhr.onload = () => {
         if (xhr.status >= 200 && xhr.status < 400) {
             const data = JSON.parse(xhr.response).data;
-            const img_data = data.images.downsized_medium;
+            console.log(data);
+            const img_data = data.images.fixed_height_small_still;
             const img = document.createElement('img');
 
             img.setAttribute('src', img_data.url);
             img.style.height = img_data.height + 'px';
             img.style.width = img_data.width + 'px';
             const images = document.querySelector('div.images');
-
-            if (images.lastChild) {
-                images.lastChild.remove();
-            }
-            images.appendChild(img);
+            const div = document.createElement('div');
+            div.style.position = 'relative';
+            div.style.display = 'inline-block';
+            div.style.margin = '15px';
+            div.innerHTML = `
+            <span class="remove" 
+            onclick="remove(event)"
+            style="position : absolute; top : 10px; right : 10px; cursor : pointer; background-color: white;"
+            >X</span>
+            `;
+            div.appendChild(img);
+            images.appendChild(div);
         } else {
             console.log(xhr.response);
             alert('OOPS! Somethont went wrong');
         }
     }
+}
+
+function remove(e) {
+    e.target.parentNode.remove();
+}
+
+function removeAll(){
+    const images = document.querySelector('div.images');
+   while(images.childElementCount > 0){
+      images.lastChild.remove();
+   }
 }
