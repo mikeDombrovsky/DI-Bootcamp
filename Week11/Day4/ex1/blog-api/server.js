5a77f120ed65266f2b8996832fd0ae2d988acca5
Mikhail Dombrovsky <madombrovsky@gmail.com>
2023-09-24 16:29:32 +0300

    week11 day4 ExercisesXP ex1

--- Week11/Day4/ex1/blog-api/server.js
+++ Week11/Day4/ex1/blog-api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 const data = [
     {
@@ -31,6 +32,54 @@ app.get('/posts/:id', (req, res) => {
         res.json(post);
     } else {
         res.status(404).send('post not found');
+    }
+});
+
+app.post('/posts', (req, res) => {
+    if (req.body.title && req.body.content) {
+        const post = {
+            id: data[data.length - 1].id + 1,
+            title: req.body.title,
+            content: req.body.content
+        }
+        data.push(post);
+        res.status(201).json(post);
+    } else {
+        res.status(400).json({ error: 'wrong body content' });
+    }
+});
+
+app.put('/posts/:id', (req, res) => {
+    const post = findPost(req.params.id);
+
+    if (post && req.body.title && req.body.content) {
+        post.title = req.body.title;
+        post.content = req.body.content;
+        res.json(post);
+
+    } else if (!post) {
+        res.status(404).json({ error: 'post not found' });
+
+    } else {
+        res.status(400).json({ error: 'wrong body content' });
+    }
+});
+
+app.delete('/posts/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(typeof id);
+
+    if (isNaN(id)) {
+        res.status(400).json({ error: 'parameter is not a number' });
+        return;
+    }
+    const i = data.findIndex(post => post.id == id);
+
+    if (i > 0) {
+        const deleted = data.splice(i, 1);
+        res.status(200).json({ msg: `post with id ${id} was deleted`, deleted });
+    } else {
+        res.status(400).json({ error: 'wrong id' });
     }
 });
 
