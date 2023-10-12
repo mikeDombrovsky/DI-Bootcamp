086866e81f06bcf6aae66ac604832cfd95190fc3
Mikhail Dombrovsky <madombrovsky@gmail.com>
2023-10-12 16:17:02 +0300

    week11 day4 DailyChallenge

--- Week11/Day4/DailyChallenge/public/script.js
+++ Week11/Day4/DailyChallenge/public/script.js
@@ -10,61 +10,6 @@ const used_emoji_ids = [];
 const start_btn = document.getElementById('start');
 start_btn.addEventListener('click', startGame);
 
-
-async function postEmoji(e) {
-    e.preventDefault();
-    const { id, value } = e.target.emoji_name;
-    console.log(id, value);
-    const resp = await fetch('/emojis/' + id,
-        {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ name: value })
-        });
-    const result = await resp.json();
-    if (result.isTrue) {
-        showTrueAnswer();
-    }
-
-    console.log(result);
-    if (used_emoji_ids.length === 30) {
-        stopGame();
-    } else {
-        showNextEmoji();
-    }
-}
-
-function showTrueAnswer() {
-    score++;
-    const scoreSpan = document.getElementById('score');
-    scoreSpan.innerText = score;
-}
-
-function stopGame(){
-    
-}
-
-async function showNextEmoji() {
-    const id = getRandomId();
-    if (!id) {
-        return;
-    }
-    const { character, names } = emojis.find(emoji => emoji.id === id);
-    const [name1, name2, name3, name4] = names;
-    const newHtml = `
-        <span id="emoji">${character}</span>
-        <select name="emoji_name" id="${id}">
-            <option value="${name1}">${name1}</option>
-            <option value="${name2}">${name2}</option>
-            <option value="${name3}">${name3}</option>
-            <option value="${name4}">${name4}</option>
-        </select>
-    `;
-    document.querySelector('div#changeable').innerHTML = newHtml;
-}
-
 async function fetchEmojis() {
     const res = await fetch('/emojis');
     const emojiArr = await res.json();
@@ -81,14 +26,85 @@ function startGame() {
     showNextEmoji();
 }
 
+async function postEmoji(e) {
+    e.preventDefault();
+    showSpinner();
+    const { id, value } = e.target.emoji_name;
+    const resp = await fetch('/emojis/' + id,
+        {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ name: value })
+        });
+    const result = await resp.json();
+    hideSpinner();
+    if (result.isTrue) {
+        clapping_hands();
+        updateScore();
+    }
+
+    console.log(result);
+    console.log(used_emoji_ids.length);
+    if (used_emoji_ids.length === 29) {
+        stopGame();
+    } else {
+        showNextEmoji();
+    }
+}
+
+function clapping_hands(){
+    const hands = document.querySelector('div.clapping_hands');
+    hands.style.display = 'block';
+    setTimeout(()=> {
+        hands.style.display = 'none';
+    }, 1000)
+}
+
+function updateScore() {
+    score++;
+    const scoreSpan = document.getElementById('score');
+    scoreSpan.innerText = score;
+}
+
+
+async function showNextEmoji() {
+    const id = getRandomId();
+    if (id === undefined) {
+        console.log('id = ', id);
+        return;
+    }
+    const { character, names } = emojis.find(emoji => emoji.id === id);
+    const [name1, name2, name3, name4] = names;
+    const newHtml = `
+        <span id="emoji">${character}</span>
+        <select name="emoji_name" id="${id}">
+            <option value="${name1}">${name1}</option>
+            <option value="${name2}">${name2}</option>
+            <option value="${name3}">${name3}</option>
+            <option value="${name4}">${name4}</option>
+        </select>
+    `;
+    document.querySelector('div#changeable').innerHTML = newHtml;
+}
+
+function stopGame() {
+    form.style.display = 'none';
+    start_btn.style.display = 'block';
+}
+
+
 function showSpinner() {
-    const spinner = document.getElementById('spinner');
-    spinner.style.display = 'inline-block';
+    const cover = document.querySelector('div.cover');
+    cover.style.display = 'inline-block';
 }
 
 function hideSpinner() {
-    const spinner = document.getElementById('spinner');
-    spinner.style.display = 'none';
+    setTimeout(() => {
+        const cover = document.querySelector('div.cover');
+        cover.style.display = 'none';
+    }, 200);
 }
 
 function getRandomId() {
@@ -102,7 +118,7 @@ function getRandomId() {
     let rand;
 
     do {
-        rand = Math.floor(Math.random() * (emojis.length - 1));
+        rand = Math.floor(Math.random() * emojis.length);
     }
     while (used_emoji_ids.includes(rand));
 
