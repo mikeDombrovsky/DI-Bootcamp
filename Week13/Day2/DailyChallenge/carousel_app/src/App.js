21f3c2905f205e14013d61f41bd84dfcadceb09a
Mikhail Dombrovsky <madombrovsky@gmail.com>
2023-10-24 17:56:56 +0300

    week13 day2 DailyChallenge

---
+++ Week13/Day2/DailyChallenge/carousel_app/src/App.js
@@ -0,0 +1,12 @@
+import MyCarousel from './components/MyCarousel';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MyCarousel/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Week13/Day2/DailyChallenge/carousel_app/src/components/MyCarousel.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+class MyCarousel extends React.Component {
+    render() {
+        return (
+            <Carousel>
+                <div>
+                    <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/jrfyzvgzvhs1iylduuhj.jpg" />
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                    <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/c1cklkyp6ms02tougufx.webp" />
+                    <p className="legend">Legend 2</p>
+                </div>
+                <div>
+                    <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/e8fnw35p6zgusq218foj.webp" />
+                    <p className="legend">Legend 3</p>
+                </div>
+                <div>
+                    <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/liw377az16sxmp9a6ylg.webp" />
+                    <p className="legend">Legend 4</p>
+                </div>
+            </Carousel>
+        );
+    }
+};
+
+export default MyCarousel;
