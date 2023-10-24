0d5954ecaab4c8c114a2fadadff703ca657d6eed
Mikhail Dombrovsky <madombrovsky@gmail.com>
2023-10-24 17:19:29 +0300

    week13 day2 ExercisesXP ex3

--- Week13/Day2/ExercisesXP/xp_app/src/components/Exercise3.js
+++ Week13/Day2/ExercisesXP/xp_app/src/components/Exercise3.js
@@ -2,16 +2,27 @@ import React from 'react';
 import './Exercise3.css'
 import cat from '../img/cat.jpg'
 
+
 class Exercise extends React.Component {
-    render(){
+
+
+    render() {
+        //part 2, change style of h1 to inline one
+        const style_header = {
+            color: "white",
+            backgroundColor: "DodgerBlue",
+            padding: "10px",
+            fontFamily: "Arial"
+        };
+ 
         return (
             <div>
-                <h1 className='ex3'>h1 from ex3</h1>
-                <p>Paragraph from ex3</p>
+                <h1 className='ex3' style={style_header}>h1 from ex3</h1>
+                <p className='para' >Paragraph from ex3</p>
                 <a href='#'>Link from ex3</a>
                 <form>
-                    <input type='text'/>
-                    <input type='submit'/>
+                    <input type='text' />
+                    <input type='submit' />
                 </form>
                 <img src={cat} width='200px'></img>
                 <ul>List:
