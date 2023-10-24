34b19715d3b45ca8170114e23793ad3d31006a7d
Mikhail Dombrovsky <madombrovsky@gmail.com>
2023-10-24 18:00:21 +0300

    week13 day2 DailyChallenge

--- Week13/Day2/DailyChallenge/carousel_app/src/components/MyCarousel.js
+++ Week13/Day2/DailyChallenge/carousel_app/src/components/MyCarousel.js
@@ -8,19 +8,19 @@ class MyCarousel extends React.Component {
             <Carousel>
                 <div>
                     <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/jrfyzvgzvhs1iylduuhj.jpg" />
-                    <p className="legend">Legend 1</p>
+                    <p className="legend">Hong Kong</p>
                 </div>
                 <div>
                     <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/c1cklkyp6ms02tougufx.webp" />
-                    <p className="legend">Legend 2</p>
+                    <p className="legend">Macao</p>
                 </div>
                 <div>
                     <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/e8fnw35p6zgusq218foj.webp" />
-                    <p className="legend">Legend 3</p>
+                    <p className="legend">Japan</p>
                 </div>
                 <div>
                     <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/liw377az16sxmp9a6ylg.webp" />
-                    <p className="legend">Legend 4</p>
+                    <p className="legend">Las Vegas</p>
                 </div>
             </Carousel>
         );
