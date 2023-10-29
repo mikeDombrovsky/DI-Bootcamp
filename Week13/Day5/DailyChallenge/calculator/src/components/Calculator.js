db46056848e87cf7a849c0dd953a46f0926e41df
Mikhail Dombrovsky <madombrovsky@gmail.com>
2023-10-29 10:23:03 +0200

    week13 day5 dailyChallenge

--- Week13/Day5/DailyChallenge/calculator/src/components/Calculator.js
+++ Week13/Day5/DailyChallenge/calculator/src/components/Calculator.js
@@ -7,8 +7,10 @@ class Calculator extends Component {
         this.state = {
             left_num: 0,
             right_num: 0,
+            operation: 'add',
             sum: 0
         }
+        console.log(this.state);
     }
 
     setLeft = (e) => {
@@ -19,8 +21,44 @@ class Calculator extends Component {
         this.setState({ right_num: e.target.value })
     }
 
-    sum = () => {
+    setOperation = (e) => {
+        this.setState({ operation: e.target.value })
+    }
+
+    operate = () => {
+        console.log(this.state.operation);
+        switch (this.state.operation) {
+            case 'add': this.add();
+                break;
+            case 'subtract': this.subtract();
+                break;
+            case 'multiply': this.multiply();
+                break;
+            case 'divide': this.divide();
+                break;
+            default: console.log('wrong operation');
+        }
+    }
+
+    add = () => {
         this.setState({ sum: Number(this.state.left_num) + Number(this.state.right_num) })
+    }
+
+    subtract = () => {
+        this.setState({ sum: Number(this.state.left_num) - Number(this.state.right_num) })
+    }
+
+
+    multiply = () => {
+        this.setState({ sum: Number(this.state.left_num) * Number(this.state.right_num) })
+    }
+
+    divide = () => {
+        if (Number(this.state.right_num) == 0){
+            alert('cannot divide by 0');
+            return;
+        }
+        this.setState({ sum: Number(this.state.left_num) / Number(this.state.right_num) })
     }
 
     render() {
@@ -31,13 +69,16 @@ class Calculator extends Component {
                     <input type="number" value={this.state.left_num} onChange={this.setLeft} />
                     <input type="number" value={this.state.right_num} onChange={this.setRight} />
                 </form>
-                <select>
+                <div className="select_operation">
+                    <select onChange={this.setOperation}>
                     <option value="add">+</option>
                     <option value="subtract">-</option>
-                    <option value="mutiply=">*</option>
-                    <option value="divide=">/</option>
+                    <option value="multiply">*</option>
+                    <option value="divide">/</option>
                 </select>
-                <button onClick={this.sum}>Add Them!</button>
+                </div>
+                
+                <button onClick={this.operate}>{this.state.operation} them!</button>
                 <div id="result">{this.state.sum}</div>
             </>
         )
