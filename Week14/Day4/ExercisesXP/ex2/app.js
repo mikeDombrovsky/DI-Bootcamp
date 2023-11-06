f8c5acea9ea38f11914503909d9efe6a0315e41a
Mikhail Dombrovsky <madombrovsky@gmail.com>
2023-11-06 11:15:52 +0200

    week14 day4  ExercisesXP ex3

--- Week14/Day4/ExercisesXP/ex2/app.js
+++ Week14/Day4/ExercisesXP/ex2/app.js
@@ -1,7 +1,9 @@
 import express from "express";
+import { todos_router } from "./routes/todos.route.js";
 
 const app = express();
 
 app.use(express.json());
+app.use("/todos",todos_router);
 
 app.listen(3000, (err) => console.log(err ? err : "listening on port 3000"));
--- Week14/Day4/ExercisesXP/ex2/routes/todos.route.js
+++ Week14/Day4/ExercisesXP/ex2/routes/todos.route.js
@@ -2,6 +2,66 @@ import { Router } from "express";
 
 const todos_router = Router();
 
+const todos = [];
+
 todos_router.get("/", (req, res) => {
-  res.status(200).json({ msg: "ok" });
+  res.status(200).json(todos);
 });
+
+todos_router.post("/", (req, res) => {
+  const { title } = req.body;
+  if (!title) {
+    return res.status(400).json({ error: "something missed" });
+  }
+  try {
+    todos.push({ id: todos.length + 1, title });
+    res.status(200).json(todos);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "server error" });
+  }
+});
+
+todos_router.put("/:id", (req, res) => {
+  const { title } = req.body;
+  const { id } = req.params;
+
+  if (!title || !id) {
+    return res.status(400).json({ error: "wrong request params" });
+  }
+
+  const todo = todos.find((todo) => todo.id === Number(id));
+  if (!todo) {
+    return res.status(404).json({ error: "todo not found" });
+  }
+
+  try {
+    todo.title = title;
+    res.status(200).json(todos);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "server error" });
+  }
+});
+
+todos_router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  if (isNaN(id)) {
+    return res.status(400).json({ error: "wrong request params" });
+  }
+
+  const i = todos.findIndex((todo) => todo.id === Number(id));
+  if (i < 0) {
+    return res.status(404).json({ error: "todo not found" });
+  }
+
+  try {
+    todos.splice(i, 1);
+    res.status(200).json(todos);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "server error" });
+  }
+});
+
+export { todos_router };
