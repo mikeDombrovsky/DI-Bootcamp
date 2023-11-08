86e8ba34b9edd7885d2b30cbf6ecfcdd8c64a93c
Mikhail Dombrovsky <madombrovsky@gmail.com>
2023-11-09 01:00:01 +0200

    week15 day2 Daily Challenge

--- Week15/Day2/DailyChallenge/task_manager/src/components/Todo.js
+++ Week15/Day2/DailyChallenge/task_manager/src/components/Todo.js
@@ -1,10 +1,17 @@
+import { useState } from "react";
 
-export const Todo = () => {
-    return (
-      <li key={todo.id}>
-        {todo.text}
-        <button onClick={() => handleRemoveTodo(todo.id)}>Remove</button>
-        <button onClick={() => handleRemoveTodo(todo.id)}>Edit</button>
-      </li>
-    );
-}
+export const Todo = ({ todo }) => {
+  const [isEdited, setIsEdited] = useState(false);
+
+  return (
+    <li key={todo.id} onClick={() => setIsEdited(!isEdited)}>
+      {/* {isEdited ? (
+        todo.text
+      ) : (
+        <input onChange={(e) => setEditText(e.target.value)} value={todo.text}/>
+      )}
+      <button onClick={() => handleRemoveTodo(todo.id)}>Remove</button>
+      <button onClick={() => handleRemoveTodo(todo.id)}>Edit</button> */}
+    </li>
+  );
+};
--- Week15/Day2/DailyChallenge/task_manager/src/components/TodoList.js
+++ Week15/Day2/DailyChallenge/task_manager/src/components/TodoList.js
@@ -1,14 +1,26 @@
 import React, { useReducer, useState } from "react";
+import { Todo } from "./Todo";
 
 // Reducer function
 function todoReducer(state, action) {
   switch (action.type) {
     case "ADD_TODO":
       // Add a new todo to the state
-      return [...state, { id: Date.now(), text: action.text }];
+      return [
+        ...state,
+        { id: Date.now(), text: action.text, status: "active" },
+      ];
     case "REMOVE_TODO":
       // Remove a todo by its id
       return state.filter((todo) => todo.id !== action.id);
+    case "EDIT_TODO":
+      return state.map((todo) => {
+        if (todo.id === action.id) {
+          todo.text = action.text;
+          return todo;
+        }
+        return todo;
+      });
     default:
       return state;
   }
@@ -17,6 +29,8 @@ function todoReducer(state, action) {
 export function TodoList() {
   const [todos, dispatch] = useReducer(todoReducer, []);
   const [todoText, setTodoText] = useState("");
+  const [editText, setEditText] = useState("");
+  const [isEditActive, setIsEditActive] = useState(false);
 
   const handleAddTodo = () => {
     if (todoText.trim() === "") return;
@@ -26,6 +40,18 @@ export function TodoList() {
 
   const handleRemoveTodo = (id) => {
     dispatch({ type: "REMOVE_TODO", id });
+  };
+
+  const handleEditTodo = (id) => {
+    if (editText.trim() === "") return;
+    dispatch({ type: "EDIT_TODO", text: editText, id });
+    setEditText("");
+    setIsEditActive(false);
+  };
+
+  const handleShowInput = () => {
+    setIsEditActive(true);
+    setEditText(todoText);
   };
 
   return (
@@ -40,11 +66,22 @@ export function TodoList() {
       <button onClick={handleAddTodo}>Add Todo</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>
-            {todo.text}
-            <button onClick={() => handleRemoveTodo(todo.id)}>Remove</button>
-            <button onClick={() => handleRemoveTodo(todo.id)}>Edit</button>
-          </li>
+          <>
+            <li key={todo.id} onClick={handleShowInput}>
+              {isEditActive ? (
+                <input
+                  onChange={(e) => setEditText(e.target.value)}
+                  value={editText}
+                />
+              ) : (
+                todo.text
+              )}
+
+              <button onClick={() => handleRemoveTodo(todo.id)}>Remove</button>
+              <button onClick={() => handleEditTodo(todo.id)}>Edit</button>
+            </li>
+            {/* <Todo /> */}
+          </>
         ))}
       </ul>
     </div>
