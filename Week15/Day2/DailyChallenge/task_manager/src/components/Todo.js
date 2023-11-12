f37412fbbd855fc714ee175b0b9fcf2cdb889afe
Mikhail Dombrovsky <madombrovsky@gmail.com>
2023-11-12 11:21:04 +0200

    week15 day2 daily challenge

--- Week15/Day2/DailyChallenge/task_manager/src/components/Todo.js
+++ Week15/Day2/DailyChallenge/task_manager/src/components/Todo.js
@@ -1,57 +1,38 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 export const Todo = ({ todo, handleEditTodo, handleRemoveTodo }) => {
   const [isBeingEdited, setIsBeingEdited] = useState(false);
   const [editText, setEditText] = useState(todo.text);
-  const [status, setStatus] = useState("active");
-  const [checked, setChecked] = useState(false);
-  const checkboxRef = useRef();
-  const spanRef = useRef();
 
   useEffect(() => {
-    console.log("status:", status, "checked:", checked);
     console.log(todo);
-    console.log('-------');
-  }, [checked, status]);
+  });
 
   const edit = (e, text) => {
     if (e.key === "Enter") {
-      handleEditTodo(todo.id, text, status);
+      handleEditTodo(todo.id, text, todo.status);
       setIsBeingEdited(false);
     }
   };
 
   const editStatus = (isChecked) => {
-    setChecked(isChecked);
     if (isChecked) {
-      setStatus("completed");
       handleEditTodo(todo.id, editText, "completed");
     } else {
-      setStatus("active");
       handleEditTodo(todo.id, editText, "active");
     }
-    console.log(
-      "status:",
-      status,
-      "checked:",
-      checked,
-      "isChecked:",
-      isChecked
-    );
-    console.log(todo);
   };
 
-  const showInput = (e) => {
+  const showInput = () => {
     setIsBeingEdited(true);
   };
 
   return (
     <li key={todo.id} onKeyDown={(e) => edit(e, editText)}>
       <input
-        ref={checkboxRef}
         type="checkbox"
         onChange={(e) => editStatus(e.target.checked)}
-        checked={checked}
+        checked={todo.status === "completed" ? true : false}
       />
       {isBeingEdited ? (
         <input
@@ -61,9 +42,7 @@ export const Todo = ({ todo, handleEditTodo, handleRemoveTodo }) => {
         />
       ) : (
         <>
-          <span ref={spanRef} onClick={showInput}>
-            {todo.text}
-          </span>
+          <span onClick={showInput}>{todo.text}</span>
         </>
       )}
       <button onClick={() => handleRemoveTodo(todo.id)}>Remove</button>
