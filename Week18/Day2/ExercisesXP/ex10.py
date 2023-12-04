697bc33a782a8109057f1d2051f1b94501b8c50e
Mikhail Dombrovsky <madombrovsky@gmail.com>
2023-12-04 21:11:25 +0200

    ExercisesXP ex11

---
+++ Week18/Day2/ExercisesXP/ex10.py
@@ -0,0 +1,9 @@
+numbers = range(1, 20)
+for i in numbers:
+    print(i)
+    
+    
+for i, num in enumerate(numbers):
+    if i % 2 == 0:
+        print(num)
+    
---
+++ Week18/Day2/ExercisesXP/ex9.py
@@ -0,0 +1,6 @@
+num = 1.5
+floats = []
+while num <= 5:
+    floats.append(num)
+    num += 0.5
+print(floats)
