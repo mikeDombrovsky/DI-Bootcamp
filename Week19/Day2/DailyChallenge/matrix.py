9c3e31d1bc216df5a47774358e6d6428a6f2f480
Mikhail Dombrovsky <madombrovsky@gmail.com>
2023-12-10 23:20:01 +0200

    week19day1

---
+++ Week19/Day2/DailyChallenge/matrix.py
@@ -0,0 +1,16 @@
+
+matrix = """7ii
+Tsx
+h%?
+i #
+sM 
+$a 
+#t%
+^r!"""
+columns = matrix.split('\n')
+for row in columns: 
+    print(row,sep='')
+    one, two, three = row
+    
+    for symbol in row:
+        pass
