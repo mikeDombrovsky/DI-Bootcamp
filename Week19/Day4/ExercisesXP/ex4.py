600f23f6eb4cabb15c6c8ea07448c69f11c36ab4
Mikhail Dombrovsky <madombrovsky@gmail.com>
2023-12-13 21:38:35 +0200

    week19 day4 exercisesXP ex4

--- Week19/Day4/ExercisesXP/ex4.py
+++ Week19/Day4/ExercisesXP/ex4.py
@@ -1,10 +1,18 @@
 # If X=3, the output when calling our function should be 3702 (3 + 33 + 333 + 3333)
-def sum(x):
-    result = x
-    a = str(x)
+def sum(number):
+    result = number
+    summand = str(number)
     for _ in range(3):
-         a += str(x)
-         result += int(a)
+         summand += str(number)
+         result += int(summand)
+    print(result)
+    
+def sum2(number):
+    result = 0
+    summand = str(number)
+    for i in range(1, 5):
+        result += int(summand * i)
     print(result)
     
 sum(3)
+sum2(3)
