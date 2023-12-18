ef5cf25572fb4b1b5918dd362edd0093d37ff56c
Mikhail Dombrovsky <madombrovsky@gmail.com>
2023-12-18 17:00:30 +0200

    week20 day2 exercisesXP ex1

--- Week20/Day2/ExercisesXP/ex1.py
+++ Week20/Day2/ExercisesXP/ex1.py
@@ -3,12 +3,15 @@ class Cat:
         self.name = cat_name
         self.age = cat_age
 
+
 murzik = Cat('Murzik', 1)
 antistress = Cat('Antis', 2)
 bibi = Cat('Bibi', 3)
 
-def find_oldest(*cats):
-    sorted_by_age = sorted( lambda cat: cat.age , cats, reverse=True)
+
+def find_oldest(cats):
+    sorted_by_age = sorted(cats, key=lambda cat: cat.age, reverse=True)
     return sorted_by_age[0]
 
-        
+oldest_cat = find_oldest([murzik, antistress, bibi])
+print(f'The oldest cat is {oldest_cat.name}, and is {oldest_cat.age} years old.')
