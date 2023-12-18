55073fa8be58f56764a89a065ebc6c05a66aeeb4
Mikhail Dombrovsky <madombrovsky@gmail.com>
2023-12-18 14:42:03 +0200

    week20 day2 daily Challenge

--- Week20/Day2/DailyChallenge/mac_donalds_farm.py
+++ Week20/Day2/DailyChallenge/mac_donalds_farm.py
@@ -1,3 +1,4 @@
+
 class Farm:
 
     def __init__(self, farmer_name):
@@ -17,10 +18,34 @@ class Farm:
             message += f'{animal} : {number}\n'
         message += '     E-I-E-I-0!'
         return message
-    
+
     def get_animal_types(self):
-        pass
-        
+        return sorted(self.animals.keys())
+
+    def get_short_info(self):
+        animals_types = list(map(self.add_s_if_many, self.get_animal_types()))
+
+        message = f'{self.farmer_name}’s farm has '
+
+        for i, animal in enumerate(animals_types):
+            if len(animals_types) == 1:
+                return f'{message} {animal}'
+            if len(animals_types) == 2:
+                return f'{message} {animal} and {animals_types[i + 1]}'
+
+            if i == len(animals_types) - 1:
+                message += f' and {animal}.'
+            else:
+                message += animal
+                if i < len(animals_types) - 2:
+                    message += ', '
+
+        return message
+
+    def add_s_if_many(self, animal):
+        if self.animals.get(animal) > 1:
+            return animal + 's'
+        return animal
 
 
 macdonald = Farm("McDonald")
@@ -28,4 +53,7 @@ macdonald.add_animal('cow', 5)
 macdonald.add_animal('sheep')
 macdonald.add_animal('sheep')
 macdonald.add_animal('goat', 12)
+macdonald.add_animal('dog', 3)
 print(macdonald.get_info())
+print(macdonald.get_animal_types())
+print(macdonald.get_short_info())
