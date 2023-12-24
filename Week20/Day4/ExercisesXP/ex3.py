7f0fc5806c3b060f67c3689d904a855f389e6c46
Mikhail Dombrovsky <madombrovsky@gmail.com>
2023-12-24 08:24:39 +0200

    week20 day4 ExercisresXP ex4

--- Week20/Day4/ExercisesXP/ex3.py
+++ Week20/Day4/ExercisesXP/ex3.py
@@ -1,6 +1,36 @@
+import random
 from ex2 import Dog
 
 
 class PetDog(Dog):
-    pass
+    def __init__(self, name, age, weight):
+        super().__init__(name, age, weight)
+        self.trained = False
 
+    def train(self):
+        print(self.bark())
+        self.trained = True
+
+    def play(self, *args):
+        for dog in args:
+            print(f'{dog}', end=' ')
+        print(f'and {self.name} all play together')
+
+    def do_a_trick(self):
+        if self.trained:
+            trick = random.choice(
+                [
+                    'does a barrel roll',
+                    'stands on his back legs',
+                    'shakes your hand',
+                    'plays dead'
+                ])
+            print(f'{self.name} {trick}')
+
+
+bobik = PetDog('Bobik', 2, 20)
+tuzik = PetDog('Tuzik', 1, 30)
+
+bobik.play('Tuzik')
+tuzik.train()
+tuzik.do_a_trick()
