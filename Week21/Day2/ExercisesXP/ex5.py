af78577576dccf80d7d75df12ac6b27ba662d685
Mikhail Dombrovsky <madombrovsky@gmail.com>
2023-12-26 14:08:26 +0200

    week21 day2 exercises xp ex5

--- Week21/Day2/ExercisesXP/ex5.py
+++ Week21/Day2/ExercisesXP/ex5.py
@@ -1,2 +1,13 @@
-import datetime
+import datetime as dt
 
+now = dt.datetime.now()
+
+jen_1 = dt.datetime(now.year,1, 1)
+
+
+if(jen_1 < now):
+    jen_1 = dt.datetime(now.year + 1, 1, 1)
+    
+time_left = jen_1 - now
+
+print(f'the 1st of January is in {time_left.days} days, and {time_left.seconds // 60 // 60} hours')
