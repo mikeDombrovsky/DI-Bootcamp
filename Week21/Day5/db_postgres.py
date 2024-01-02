64a89338f36f6c3e7cc26a628fa8a8d7f91e82a7
Mikhail Dombrovsky <madombrovsky@gmail.com>
2024-01-02 02:22:52 +0200

    week21 day5 xp ex1

--- Week21/Day5/db_postgres.py
+++ Week21/Day5/db_postgres.py
@@ -10,23 +10,58 @@ DB_PASS = os.getenv('DB_PASS')
 DB_NAME = os.getenv('DB_NAME')
 DB_PORT = os.getenv('DB_PORT')
 
-connection = psycopg2.connect(
-    host=DB_HOST,
-    user=DB_USERNAME,
-    password=DB_PASS, 
-    dbname=DB_NAME,
-    port=DB_PORT)
+params = {
+    "host": DB_HOST,
+    "database": DB_NAME,
+    "user": DB_USERNAME,
+    "password": DB_PASS,
+    "port": DB_PORT
+}
 
-cursor = connection.cursor()  # obj that runs queries like pgAdmin query tool.
+def set_up():
 
-query = 'SELECT * FROM users LIMIT 2'
+    query = '''CREATE TABLE IF NOT EXISTS menu_items(
+        item_id SERIAL PRIMARY KEY,
+        item_name VARCHAR(30) NOT NULL,
+        item_price SMALLINT DEFAULT 0
+    )'''
 
-cursor.execute(query)
+    results_list = execute_query(query)
 
-results_list = cursor.fetchall()
+    for row in results_list:
+        print(row)
 
-connection.close()
 
-for row in results_list:
-    print(row)
-    
+
+def execute_query(query, is_select=True):
+    try:
+        connection = psycopg2.connect(
+            host=DB_HOST,
+            user=DB_USERNAME,
+            password=DB_PASS,
+            dbname=DB_NAME,
+            port=DB_PORT)
+
+        # obj that runs queries like pgAdmin query tool.
+        cursor = connection.cursor()
+
+        cursor.execute(query)
+        if(is_select):
+            return cursor.fetchall()
+        return connection.commit()
+        
+
+    except (Exception, psycopg2.Error) as e:
+        print(e)
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+
+
+def get_connection(params=params):
+    try:
+        return psycopg2.connect(
+            **params)
+    except (Exception, psycopg2.Error) as e:
+        print(e)
