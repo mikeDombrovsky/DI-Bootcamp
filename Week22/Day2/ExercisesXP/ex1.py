814e15915a25a84070476e260887ee1ca4493462
Mikhail Dombrovsky <madombrovsky@gmail.com>
2024-01-09 00:21:44 +0200

    week22  day 2 xp

---
+++ Week22/Day2/ExercisesXP/ex1.py
@@ -0,0 +1,14 @@
+# O(n)
+# for i in range(10):
+#     print(i)
+
+# O(n^2)
+# for i in range(n):
+#     for j in range(n):
+#         print(i, j)
+        
+# O(log(n))
+i = 1
+while i < n:
+    i *= 2
+    print(i)
