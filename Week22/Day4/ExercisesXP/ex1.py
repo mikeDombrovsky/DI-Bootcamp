b600bfb0f91e856300de5b24b23165f4fd58da0b
Mikhail Dombrovsky <madombrovsky@gmail.com>
2024-01-11 23:36:28 +0200

    week22 day4 ex1

--- Week22/Day4/ExercisesXP/ex1.py
+++ Week22/Day4/ExercisesXP/ex1.py
@@ -1,9 +1,46 @@
+from my_queue import Queue
+
 def main():
-    pass
+    tasks = Queue()
+    
+    while True:
+        command = show_menu()
+        
+        if command == 'Q':
+            return print('Good bye! <3 <3 <3')
+        if command == 'T':
+            try:
+                task = get_task()
+                tasks.add_task(task)
+            except Exception as e:
+                print('OOOPS...')
+                print(e)
+        if command == 'E':
+            next = tasks.next()
+            print('next:', next)
+            if next:
+                return print(next)
+            print('Queue is empty')
+        if command == 'S':
+            print(f'Task Queue: {tasks}')
 
-def add_task(task, priority):
-    pass
+def show_menu():
+    return input('''
+-------------------------
+|  Add task -------(T)  |
+|  Execute task----(E)  |
+|  Show task queue-(S)  |
+|  Exit------------(Q)  |
+-------------------------
+''').upper()
+    
+    
+def get_task():
+    task =  input('Task: ')
+    priority = int(input('Priority: '))
+    return task, priority
 
-def execute_next_task():
-    pass
 
+
+if __name__ == '__main__':
+    main()
---
+++ Week22/Day4/ExercisesXP/my_queue.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+
+class Queue:
+    def __init__(self) -> None:
+        self.tasks = deque()
+
+    def next(self):
+        if len(self.tasks) > 0:
+            return self.tasks.pop()
+
+    def add_task(self, task_info):
+        task, priority = task_info
+        if priority == 1:
+            self.tasks.append((task, 1))
+        elif priority == 2:
+            self.tasks.appendleft((task, 2))
+        else:
+            raise ValueError('Priority must be number - 1 or 2')
+
+    def __str__(self) -> str:
+
+        string = '['
+        for i in self.tasks:
+            string += f'{i},'
+
+        return string[:-1] + ']'
