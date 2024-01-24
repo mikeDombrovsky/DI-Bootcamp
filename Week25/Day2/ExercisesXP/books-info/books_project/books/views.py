774764dae9d95bde0bead241bf17a2ff4ba9926a
Mikhail Dombrovsky <madombrovsky@gmail.com>
2024-01-25 01:01:12 +0200

    day2 xp

--- Week25/Day2/ExercisesXP/books-info/books_project/books/views.py
+++ Week25/Day2/ExercisesXP/books-info/books_project/books/views.py
@@ -1,5 +1,19 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from models import Book
+from django.views.decorators.csrf import csrf_exempt
 
 def test(request):
     return HttpResponse('<h1>Test</h1>')
+
+
+def list_books(request):
+    return JsonResponse(Book.objects.all())
+
+
+def book_detail(request, id):
+    return JsonResponse(Book.objects.get(id=id))
+
+@csrf_exempt
+def create_book(request):
+    pass
--- Week25/Day2/ExercisesXP/books-info/books_project/books_project/settings.py
+++ Week25/Day2/ExercisesXP/books-info/books_project/books_project/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,11 +81,11 @@ WSGI_APPLICATION = 'books_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'books_xp',
-        'USER': 'postgres',  # postgres
-        'PASSWORD': '3446',
-        'HOST': 'localhost',  # localhost
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),  # postgres
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),  # localhost
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
